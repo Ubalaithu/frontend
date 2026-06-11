ead76f71484935cb74d4a87a36051735211784c8
Luba Jean Sibanda <ljsibz1234@gmail.com>
2026-06-11 20:57:19 +0200

    final

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001',
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('steakz_token');
-  if (token) config.headers['Authorization'] = `Bearer ${token}`;
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
   return config;
 });
 
